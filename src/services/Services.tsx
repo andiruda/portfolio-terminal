23392fe784d7aff4dc7d3aba49d8d62d170d4b05
Andrew Ruda <andrew@dp360crm.com>
2020-12-16 17:05:48 -0600

    Add string conversion for case-insensitive matching

--- src/services/Services.tsx
+++ src/services/Services.tsx
@@ -1,7 +1,7 @@
 
 export function RunCommand(cmd:any, client:any = {}) {
   let retVal = '';
-  switch (cmd) {
+  switch (cmd.toLowerCase()) {
     case 'start' :
     case 'init' : 
     retVal = `
